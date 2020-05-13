d8156cea482e9df64a46207f6db4b90e558786ce
Roger Pence <roger.pence@gmail.com>
2020-05-13 01:18:51 -0500

    refactor and start dim

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 class FadeInFadeOutElement {
-    constructor(targetElementId, 
-                showCSSClass = 'show-fade-away',
-                secondsDurationShown = 10) {        
+    constructor(targetElementId,
+                CSSClass = 'show-fade-away',
+                secondsDurationShown = 10) {
+
         this.targetElementId = targetElementId;
         this.targetElement = document.getElementById(this.targetElementId);
         if (!this.targetElement) {
-            throw `Target element ${targetElementId} for FadeInFadeOutElement not found.`
-        }                     
+            throw `Target element ${targetElementId} for FadeInFadeOutElement not found.`;
+        }
 
+        this.CSSClass = CSSClass;
         this.secondsDurationShown= secondsDurationShown;
-        this.showCSSCLass = 'show-fade-away';
-        
+
         this.targetElement.addEventListener('click', this.hide);
     }
-    
+
     showWithDelay = (secondsBeforeShowing) => {
         setTimeout(() => {
             this.show()
-        }, secondsBeforeShowing * 1000);        
+        }, secondsBeforeShowing * 1000);
     }
 
-    show = (overrideDuration = this.secondsDurationShown) => {
-        if (this.targetElement.classList.contains(this.showCSSCLass)) {
+    show = (duration = this.secondsDurationShown) => {
+        if (this.targetElement.classList.contains(this.CSSClass)) {
             return;
         }
-        
-        this.targetElement.classList.add(this.showCSSCLass);    
-        
-        if (overrideDuration > 0) {
+
+        this.targetElement.classList.add(this.CSSClass);
+
+        if (duration > 0) {
             setTimeout(() => {
-                this.targetElement.classList.remove(this.showCSSCLass);    
-            }, overrideDuration * 1000)    
-        }            
+                this.targetElement.classList.remove(this.CSSClass);
+            }, duration * 1000)
+        }
     }
-    
+
     hide = () => {
-        this.targetElement.removeEventListener('click', this.hide); 
-        this.targetElement.classList.remove(this.showCSSCLass);    
-    }          
+        this.targetElement.removeEventListener('click', this.hide);
+        this.targetElement.classList.remove(this.CSSClass);
+        document.body.classList.remove('dim');
+    }
 }
 
 const clicker = document.getElementById('clicker');
 clicker.addEventListener('click', (e) => {
     let x = new FadeInFadeOutElement('cookies-msg');
     x.show(-1);
+    document.body.classList.add('dim');
     // (new FadeInFadeOutElement('purple-box')).show(16);
+});
+
+const btn = document.getElementById('my-button');
+btn.addEventListener('click', (e) => {
+    console.log('button clicked');
 });
 
 document.addEventListener('DOMContentLoaded', () => {
